3188de4a5a5ad469f22e84cbd91dece39b2aca81
Jyri-Petteri Paloposki <jyri-petteri.paloposki@iki.fi>
2015-11-27 14:19:57 +0200

    Modifications to work with a Drupal + Drush workflow

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
 FROM ubuntu:14.04
-MAINTAINER Eugene Ware <eugene@noblesamurai.com>
+MAINTAINER Jyri-Petteri Paloposki <jyri-petteri.paloposki@avoltus.com>
 
 # Keep upstart from complaining
 RUN dpkg-divert --local --rename --add /sbin/initctl
 RUN ln -sf /bin/true /sbin/initctl
 
-# Let the conatiner know that there is no tty
+# Let the container know that there is no tty
 ENV DEBIAN_FRONTEND noninteractive
 
 RUN apt-get update
@@ -15,7 +15,7 @@ RUN apt-get -y upgrade
 RUN apt-get -y install mysql-server mysql-client nginx php5-fpm php5-mysql php-apc pwgen python-setuptools curl git unzip
 
 # Wordpress Requirements
-RUN apt-get -y install php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-ming php5-ps php5-pspell php5-recode php5-sqlite php5-tidy php5-xmlrpc php5-xsl
+RUN apt-get -y install php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-ming php5-ps php5-pspell php5-recode php5-sqlite php5-tidy php5-xmlrpc php5-xsl openssh-server drush
 
 # mysql config
 RUN sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
@@ -41,14 +41,6 @@ RUN /usr/bin/easy_install supervisor
 RUN /usr/bin/easy_install supervisor-stdout
 ADD ./supervisord.conf /etc/supervisord.conf
 
-# Install Wordpress
-ADD https://wordpress.org/latest.tar.gz /usr/share/nginx/latest.tar.gz
-RUN cd /usr/share/nginx/ && tar xvf latest.tar.gz && rm latest.tar.gz
-RUN mv /usr/share/nginx/html/5* /usr/share/nginx/wordpress
-RUN rm -rf /usr/share/nginx/www
-RUN mv /usr/share/nginx/wordpress /usr/share/nginx/www
-RUN chown -R www-data:www-data /usr/share/nginx/www
-
 # Wordpress Initialization and Startup Script
 ADD ./start.sh /start.sh
 RUN chmod 755 /start.sh
@@ -56,8 +48,10 @@ RUN chmod 755 /start.sh
 # private expose
 EXPOSE 3306
 EXPOSE 80
+EXPOSE 22
 
 # volume for mysql database and wordpress install
-VOLUME ["/var/lib/mysql", "/usr/share/nginx/www"]
+#VOLUME ["/var/lib/mysql", "/usr/share/nginx/www"]
+VOLUME ["/var/lib/mysql"]
 
 CMD ["/bin/bash", "/start.sh"]
--- start.sh
+++ start.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-if [ ! -f /usr/share/nginx/www/wp-config.php ]; then
-  #mysql has to be started this way as it doesn't work to call from /etc/init.d
-  /usr/bin/mysqld_safe &
-  sleep 10s
-  # Here we generate random passwords (thank you pwgen!). The first two are for mysql users, the last batch for random keys in wp-config.php
-  WORDPRESS_DB="wordpress"
-  MYSQL_PASSWORD=`pwgen -c -n -1 12`
-  WORDPRESS_PASSWORD=`pwgen -c -n -1 12`
-  #This is so the passwords show up in logs.
-  echo mysql root password: $MYSQL_PASSWORD
-  echo wordpress password: $WORDPRESS_PASSWORD
-  echo $MYSQL_PASSWORD > /mysql-root-pw.txt
-  echo $WORDPRESS_PASSWORD > /wordpress-db-pw.txt
 
-  sed -e "s/database_name_here/$WORDPRESS_DB/
-  s/username_here/$WORDPRESS_DB/
-  s/password_here/$WORDPRESS_PASSWORD/
+#mysql has to be started this way as it doesn't work to call from /etc/init.d
+/usr/bin/mysqld_safe &
+sleep 10s
+# Here we generate random passwords (thank you pwgen!). The first two are for mysql users, the last batch for random keys in wp-config.php
+DB="database"
+MYSQL_PASSWORD=`pwgen -c -n -1 12`
+DB_PASSWORD=`pwgen -c -n -1 12`
+#This is so the passwords show up in logs.
+echo mysql root password: $MYSQL_PASSWORD
+echo wordpress password: $DB_PASSWORD
+echo $MYSQL_PASSWORD > /mysql-root-pw.txt
+echo $DB_PASSWORD > /wordpress-db-pw.txt
+
+if [ ! -f /usr/share/nginx/www/wp-config.php ]; then
+  sed -e "s/database_name_here/$DB/
+  s/username_here/$DB/
+  s/password_here/$DB_PASSWORD/
   /'AUTH_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
   /'SECURE_AUTH_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
   /'LOGGED_IN_KEY'/s/put your unique phrase here/`pwgen -c -n -1 65`/
@@ -25,32 +26,24 @@ if [ ! -f /usr/share/nginx/www/wp-config.php ]; then
   /'LOGGED_IN_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/
   /'NONCE_SALT'/s/put your unique phrase here/`pwgen -c -n -1 65`/" /usr/share/nginx/www/wp-config-sample.php > /usr/share/nginx/www/wp-config.php
 
-  # Download nginx helper plugin
-  curl -O `curl -i -s https://wordpress.org/plugins/nginx-helper/ | egrep -o "https://downloads.wordpress.org/plugin/[^']+"`
-  unzip -o nginx-helper.*.zip -d /usr/share/nginx/www/wp-content/plugins
-  chown -R www-data:www-data /usr/share/nginx/www/wp-content/plugins/nginx-helper
-
-  # Activate nginx plugin once logged in
-  cat << ENDL >> /usr/share/nginx/www/wp-config.php
-\$plugins = get_option( 'active_plugins' );
-if ( count( \$plugins ) === 0 ) {
-  require_once(ABSPATH .'/wp-admin/includes/plugin.php');
-  \$pluginsToActivate = array( 'nginx-helper/nginx-helper.php' );
-  foreach ( \$pluginsToActivate as \$plugin ) {
-    if ( !in_array( \$plugin, \$plugins ) ) {
-      activate_plugin( '/usr/share/nginx/www/wp-content/plugins/' . \$plugin );
-    }
-  }
-}
-ENDL
-
   chown www-data:www-data /usr/share/nginx/www/wp-config.php
-
-  mysqladmin -u root password $MYSQL_PASSWORD
-  mysql -uroot -p$MYSQL_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-  mysql -uroot -p$MYSQL_PASSWORD -e "CREATE DATABASE wordpress; GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'localhost' IDENTIFIED BY '$WORDPRESS_PASSWORD'; FLUSH PRIVILEGES;"
-  killall mysqld
 fi
+
+mysqladmin -u root password $MYSQL_PASSWORD
+mysql -uroot -p$MYSQL_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+mysql -uroot -p$MYSQL_PASSWORD -e "CREATE DATABASE db; GRANT ALL PRIVILEGES ON db.* TO 'db'@'localhost' IDENTIFIED BY '$DB_PASSWORD'; FLUSH PRIVILEGES;"
+killall mysqld
+
+mkdir /var/run/sshd
+mkdir -p /root/.ssh
+chmod 700 /root/.ssh
+chmod 600 /root/.ssh/*
+echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC2DpmBHGS3frXP5SySXPx6uoWYixILRJAnPtoNiuNrd5jQTdh0mbwMPtRL/VqszEGiGmhD8M3DIKExEIV6hibHE3CnBxM5pdriaWool8fzi8lcW5wDNXV/U1tiDinX1kwqyqUvvOv5dWpgIk5jdbatHSZypwt/pUpwgoNlFdJWZY5GhjcmbThby7oUFmBiC9HckEWsYfAWJOoA6HLr9v+ydAPQIzW1V3eMJ8DIrrv1yKC3Wb1pmPKmnF7D7LG36Hg5MT3QlBv+TxeeDFOkTaiyeQw8yWDueRFTMXL0g5KYO3pXbd74GdD2KYRr5tYjQfMan74eCOJlcYIWOay9KnbmP127y17OHmrgGLdfuQn4WaKxBzS/UqaPnl1LHSXHYG6r8Xjv9TDAK1LHZiQybtSFPrlSHZQBIDAAT0ixS8McY4Y8o1AOkBt8snH0YtMOgcGT83bL4WdH1q9CLZLihL7jD34aBavyXlG0pkk5NN4blaRmvhb5TUd7Bl8lZnnR8ZR2p47nA1tBMZkNRPRDmgmZW5Tv2uEOVFlcaj/u8h+6Z2xObOAyggsuED+SfXXlBysFIRGKs8gNSQsGXGVvPpJys7jK5g/QzAywFxvw4ktkAuDIqJvoAus3uGmAyjXmPwVqjxNfLyDXocXByQgK9qWq0c0maWrJ8P6Q88e27Jas6w== zeip@avoltus.com' >> ~/.ssh/authorized_keys
+chown -Rf root:root /root/.ssh
+
+# configure sshd to block authentication via password
+sed -i.bak 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
+rm /etc/ssh/sshd_config.bak
 
 # start all the services
 /usr/local/bin/supervisord -n
